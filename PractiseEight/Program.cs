81e18d50c34f446de554a4c683553ee21939cb79
30176966 <30176966@RXAS1003>
2017-09-11 10:25:59 +0100

    Add project files.

---
+++ PractiseEight/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseEight
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int Min = 0;
+            int Max = 10;
+
+            int[] RandomArray = new int[5];
+
+            int[] ArrayInts = new int[10];
+
+            Random randNum = new Random();
+            for (int i = 0; i < ArrayInts.Length; i++)
+            {
+                ArrayInts[i] = randNum.Next(Min, Max);
+                Console.WriteLine(ArrayInts[i]);
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}
